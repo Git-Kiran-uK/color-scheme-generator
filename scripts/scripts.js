72accea7647e042753c59980568716b6a898576e
Kiran <kirankanchan2000@gmail.com>
2025-01-20 07:54:34 +0530

    basic functioning color scheme generator
    Alpha build of the color scheme generator. Need to add more styles

---
+++ scripts/scripts.js
@@ -0,0 +1,24 @@
+const formEl = document.getElementById('form-1');
+const colorContainer = document.querySelectorAll('.choice-container');
+
+formEl.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(formEl);
+    const colorHex = formData.get('color').replace(/^#/, '');
+    const colorMode = formData.get('mode');
+    handleColorFetch(colorHex, colorMode);
+});
+
+async function handleColorFetch(hex, mode) {
+    const response = await fetch(`https://www.thecolorapi.com/scheme?hex=${hex}&mode=${mode}`);
+    const colorData = await response.json();
+    displayColor(colorData.colors);
+}
+
+function displayColor(colorArray){
+    for(let i = 0; i < colorContainer.length; i++){
+        const hexColor = colorArray[i].hex.value;
+        colorContainer[i].children[0].style.backgroundColor = hexColor;
+        colorContainer[i].children[1].textContent = hexColor;
+    }
+}

6f765a112f113fe1e2c63956fefc2a11cb1e7da6
Kiran <kirankanchan2000@gmail.com>
2025-01-22 17:12:57 +0530

    custom select element
    Custom select element replacing old one with bit more styling

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,24 @@
+const dropdownEl = document.querySelector('.dropdown');
+const selectedEl = document.querySelector('.dropdown-selected');
+
 const formEl = document.getElementById('form-1');
 const colorContainer = document.querySelectorAll('.choice-container');
 
+selectedEl.addEventListener('click', () => {
+    dropdownEl.classList.toggle('active');
+  });
+
 formEl.addEventListener('submit', (e) => {
     e.preventDefault();
+    getFormData();
+});
+
+function getFormData(){
     const formData = new FormData(formEl);
     const colorHex = formData.get('color').replace(/^#/, '');
     const colorMode = formData.get('mode');
     handleColorFetch(colorHex, colorMode);
-});
+}
 
 async function handleColorFetch(hex, mode) {
     const response = await fetch(`https://www.thecolorapi.com/scheme?hex=${hex}&mode=${mode}`);
@@ -22,3 +33,5 @@ function displayColor(colorArray){
         colorContainer[i].children[1].textContent = hexColor;
     }
 }
+
+getFormData();

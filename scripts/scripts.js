a2063b45e2a3a853def47389b890940c2c4e43ee
Kiran <kirankanchan2000@gmail.com>
2025-01-22 18:23:47 +0530

    styling
    Added more styling

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,20 +1,37 @@
-const dropdownEl = document.querySelector('.dropdown');
-const selectedEl = document.querySelector('.dropdown-selected');
-
-const formEl = document.getElementById('form-1');
+const dropdown = document.querySelector('.dropdown');
+const selected = document.querySelector('.dropdown-selected');
+const options = document.querySelectorAll('.dropdown-options li');
+const hiddenInput = document.getElementById('color-mode');
+const form = document.getElementById('form-1');
 const colorContainer = document.querySelectorAll('.choice-container');
 
-selectedEl.addEventListener('click', () => {
-    dropdownEl.classList.toggle('active');
-  });
+selected.addEventListener('click', () => {
+    dropdown.classList.toggle('active');
+});
 
-formEl.addEventListener('submit', (e) => {
+form.addEventListener('submit', (e) => {
     e.preventDefault();
     getFormData();
 });
 
+options.forEach(option => {
+    option.addEventListener('click', (e) => {
+        options.forEach(element => element.classList.remove('selected'));
+        option.classList.add('selected');
+        selected.innerHTML = `${option.textContent} <i class="fa-solid fa-angle-down" style="color: #9ca3af;"></i>`;
+        dropdown.classList.remove('active');
+        hiddenInput.value = e.target.dataset.value;
+    });
+});
+
+document.addEventListener('click', (e) => {
+    if (!dropdown.contains(e.target)) {
+        dropdown.classList.remove('active');
+    }
+});
+
 function getFormData(){
-    const formData = new FormData(formEl);
+    const formData = new FormData(form);
     const colorHex = formData.get('color').replace(/^#/, '');
     const colorMode = formData.get('mode');
     handleColorFetch(colorHex, colorMode);
